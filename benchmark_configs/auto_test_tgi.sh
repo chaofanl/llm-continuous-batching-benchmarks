b8494492fd0015e003296f7a29bdd044520ef73b
Chaofan Li <chaofan.li@intel.com>
2024-07-08 16:31:28 +0800

    Update auto_test_tgi.sh

--- benchmark_configs/auto_test_tgi.sh
+++ benchmark_configs/auto_test_tgi.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # set -x
-
 # test case :
 # in128 , out512-1024
 # in512 , out1-256
@@ -20,37 +19,59 @@
 # bs_arr=(16 32 64 128)
 
 # llama2-70b
-model_name_dir=/models/llama2-70b-chat
-num_shard=8
-input_mean_arr=(384 128 912)
-input_range_arr=(256 0 224)
-output_mean_arr=(384 912 128)
-output_range_arr=(256 224 0)
-dist_arr=("uniform" "uniform" "uniform")
-# data_type=bfloat16
-bs_arr=(16 32 64 128)
+# model_name_dir=/models/llama2-70b-chat
+# model_name_dir=/workspace/models--meta-llama--Llama-2-70b-chat-hf/snapshots/e1ce257bd76895e0864f3b4d6c7ed3c4cdec93e2
+# num_shard=8
+# input_mean_arr=(384 128 912)
+# input_range_arr=(256 0 224)
+# output_mean_arr=(384 912 128)
+# output_range_arr=(256 224 0)
+# dist_arr=("uniform" "uniform" "uniform")
+# data_type=("bfloat16")
+# bs_arr=(512)
 
 # single case
-# input_mean_arr=(912)
-# input_range_arr=(224)
-# output_mean_arr=(400)
-# output_range_arr=(200)
-# bs_arr=(16)
-# dist_arr=("uniform")
+# model_name_dir=/models/llama2-13b-chat/
+# model_name_dir=/nfs/chaofanl/tgi/aicse.huggingface.model.references/Baichuan2/fine-tune/baichuan2-13b-chat
+model_name_dir=/nfs/chaofanl/tgi/aicse.huggingface.model.references/Baichuan2/fine-tune/baichuan2-7b-chat
 
-for bs in "${bs_arr[@]}"
-do
-    idx=0
-    for _ in "${input_mean_arr[@]}"
+# model_name_dir=/checkpoint/hf/baichuan2-13b-chat/
+num_shard=1
+input_mean_arr=(1075)
+input_range_arr=(0)
+output_mean_arr=(200)
+output_range_arr=(0)
+bs_arr=(4)
+dist_arr=("uniform")
+data_type=("bfloat16")
+
+# num_shard=1
+# input_mean_arr=(128)
+# input_range_arr=(0)
+# output_mean_arr=(128)
+# output_range_arr=(0)
+# bs_arr=(8)
+# dist_arr=("uniform")
+# data_type=("bfloat16")
+
+
+# export DBG_TRACE_FILENAME=debug.log
+for dtype in "${data_type[@]}"
+do 
+    for bs in "${bs_arr[@]}"
     do
-        input_mean=${input_mean_arr[$idx]}
-        input_range=${input_range_arr[$idx]}
-        output_mean=${output_mean_arr[$idx]}
-        output_range=${output_range_arr[$idx]}
-        dist=${dist_arr[$idx]}
-        echo "bs:$bs input_mean:$input_mean input_range:$input_range output_mean:$output_mean range:$output_range dist:$dist card_num $num_shard"
-        bash ./tgi_gaudi_variable_size $input_mean $output_mean $output_range $bs $dist $input_range $model_name_dir $num_shard 2>&1 | tee term_log_im${input_mean}_ir${input_range}_om${output_mean}_or${output_range}_bs${bs}_cn${num_shard}
-        idx=$idx+1
+        idx=0
+        for _ in "${input_mean_arr[@]}"
+        do
+            input_mean=${input_mean_arr[$idx]}
+            input_range=${input_range_arr[$idx]}
+            output_mean=${output_mean_arr[$idx]}
+            output_range=${output_range_arr[$idx]}
+            dist=${dist_arr[$idx]}
+            echo "bs:$bs input_mean:$input_mean input_range:$input_range output_mean:$output_mean range:$output_range dist:$dist card_num $num_shard dtype $dtype"
+            bash ./tgi_gaudi_variable_size $input_mean $output_mean $output_range $bs $dist $input_range $model_name_dir $num_shard $dtype 2>&1 | tee term_log_im${input_mean}_ir${input_range}_om${output_mean}_or${output_range}_bs${bs}_cn${num_shard}_dt${dtype}
+            idx=$idx+1
+        done
     done
 done
 
